21111f05b90cbf4cf20144dd4bf48bc60c685ad7
Siddhant Bugalia <sidbugalia@gmail.com>
2025-06-19 22:12:51 +0530

    fix: fix for path resolution for ts

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,19 @@
-import express from 'express';
-import { db } from './config/db';
+import { errorHandler } from '@shared/errors/errorHandler';
+import express, { NextFunction, Request, Response } from 'express';
+import router from '@modules/contacts/contact.routes';
 
 const app = express();
 
 app.use(express.json());
+
+app.use('/identify', router);
+
+app.use((req: Request, res: Response, next: NextFunction) => {
+  res.status(404).json({ message: 'Not Found' });
+});
+
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+  errorHandler(err, req, res, next);
+});
 
 export default app;
--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,4 @@
+import 'tsconfig-paths/register';
 import app from './app';
 import { db } from './config/db';
 import { env } from './config/env';
