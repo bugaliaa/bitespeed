e66e1434235e4d099957316b8d31f5c90655810f
Siddhant Bugalia <sidbugalia@gmail.com>
2025-06-19 13:14:21 +0530

    chore: format env.ts

--- src/config/env.ts
+++ src/config/env.ts
@@ -4,12 +4,13 @@ import { z } from 'zod';
 dotenv.config();
 
 const envSchema = z.object({
-  DB_USER: z.string(),
-  DB_HOST: z.string(),
-  DB_NAME: z.string(),
-  DB_PASSWORD: z.string(),
-  DB_PORT: z.string().transform(Number),
-  APP_PORT: z.string().transform(Number),
+    DB_USER: z.string(),
+    DB_HOST: z.string(),
+    DB_NAME: z.string(),
+    DB_PASSWORD: z.string(),
+    DB_PORT: z.string().transform(Number),
+    APP_PORT: z.string().transform(Number),
 });
 
 export const env = envSchema.parse(process.env);
+export type Env = typeof env;
