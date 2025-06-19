fb2d8999f62518c83ad074c5e29c32285364e1f8
Siddhant Bugalia <sidbugalia@gmail.com>
2025-06-19 10:46:56 +0530

    chore: environment validation set up

---
+++ src/config/env.ts
@@ -0,0 +1,15 @@
+import dotenv from 'dotenv';
+import { z } from 'zod';
+
+dotenv.config();
+
+const envSchema = z.object({
+  DB_USER: z.string(),
+  DB_HOST: z.string(),
+  DB_NAME: z.string(),
+  DB_PASSWORD: z.string(),
+  DB_PORT: z.string().transform(Number),
+  APP_PORT: z.string().transform(Number),
+});
+
+export const env = envSchema.parse(process.env);
